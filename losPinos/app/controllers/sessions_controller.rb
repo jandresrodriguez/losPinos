8be054a4bf9c72dec6638f625a88dbdee3597b5e
Juan Andres Rodriguez <enanosanlo@gmail.com>
2014-03-28 16:52:58 -0300

    Sign in controller in construction

--- losPinos/app/controllers/sessions_controller.rb
+++ losPinos/app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+  	  user = AdminUser.find_by(email: params[:session][:email].downcase)
+  	  if user && user.authenticate(params[:session][:password]) && false
+      # Sign the user in and redirect to the user's show page.
+  	  else
+      # Create an error message and re-render the signin form.
+      	flash[:error] = 'Invalid email/password combination' # Not quite right!
+      	render 'new'
+  	  end
+end
+
+	def destroy
+
+	end
 end
