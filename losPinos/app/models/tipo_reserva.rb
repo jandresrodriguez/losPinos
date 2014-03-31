5181659db1ba0666edccb30a6fa9812aedc711d3
Mathias Carignani <mathcarignani@gmail.com>
2014-03-31 12:04:03 -0300

    Add find TipoReserva

--- losPinos/app/models/tipo_reserva.rb
+++ losPinos/app/models/tipo_reserva.rb
@@ -4,16 +4,4 @@ class TipoReserva < ActiveRecord::Base
 		"#{nombre}"
 	end
 
-	def self.habilitadas # Metodo de clase
-		
-		# Recorro todos los tipos de reservas y si esta habilitada la retorno
-		tipos_reservas = []
-    TipoReserva.all.each do |ts|
-    	if (ts.habilitada)
-      	tipos_reservas.push([ts.nombre, ts.id])
-      end
-    end
-		return tipos_reservas
-	end
-
 end
