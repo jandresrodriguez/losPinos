b1f64c1b26817acef4b1c5e00d06216fb2103575
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-04-11 16:45:35 -0300

    Font awesome FIXED

--- losPinos/config/application.rb
+++ losPinos/config/application.rb
@@ -13,10 +13,7 @@ module LosPinos
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.generators do |g|
-       g.test_framework  :rspec, :fixture => true, :views => false
-       g.integration_tool :rspec, :fixture => true, :views => true
-    end
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
