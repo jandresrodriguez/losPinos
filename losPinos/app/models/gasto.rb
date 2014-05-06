0fcf90b7b2999a4056e00fadd6e833ef3420b04f
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-05-06 15:59:44 -0300

    Still fixing some details

--- losPinos/app/models/gasto.rb
+++ losPinos/app/models/gasto.rb
@@ -1,2 +1,6 @@
 class Gasto < ActiveRecord::Base
+
+	validates :rubro, presence: true, allow_blank: false
+	validates :monto, presence: true, allow_blank: false
+	validates :moneda, presence: true, allow_blank: false
 end
